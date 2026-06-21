f08f6869b12fdbfbcdb12de3078c8dec0ee56561
Nitesh <niteshkjaiswal108@gmail.com>
2026-06-21 20:14:51 +0530

    Enhance text.py with additional string manipulation examples, including slicing and indexing, and demonstrate immutability of strings in Python.

--- practice/text.py
+++ practice/text.py
@@ -55,3 +55,20 @@ print(word[5])
 word = 'Nature'
 print(word[-1])
 print(word[-5])
+
+word = 'Universe'
+print(word[0:5])
+print(word[2:5])
+print(word[:2])
+print(word[4:])
+print(word[-2:])
+print(word[:2] + word[2:])
+
+# print(word[42]) # the word only has 8 characters (will give error)
+
+print(word[4:42])
+print(word[42:])
+
+# Python strings are immutable
+
+word[1] = 'h' # will give error
