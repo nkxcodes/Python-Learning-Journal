c812fd86ae5ce20c13e82c576fc062a5e576f1aa
Nitesh <niteshkjaiswal108@gmail.com>
2026-06-22 16:31:20 +0530

    Add a new file lists.py to demonstrate list usage in Python, including examples of indexing and slicing with even numbers.

---
+++ practice/lists.py
@@ -0,0 +1,10 @@
+
+# A list is like container in which we can store many values together
+
+even_numbers = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+print(even_numbers)
+
+print(even_numbers[0])
+print(even_numbers[5])
+print(even_numbers[0:6])
+print(even_numbers[5:0:-1])
