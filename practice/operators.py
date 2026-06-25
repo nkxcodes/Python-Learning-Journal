2a333fd189ccc992ce750081a3b6d118dbf32bfa
Nitesh <niteshkjaiswal108@gmail.com>
2026-06-25 19:31:02 +0530

    Add operators.py to demonstrate basic arithmetic operations in Python, including addition, subtraction, multiplication, division, floor division, modulus, and exponentiation.

---
+++ practice/operators.py
@@ -0,0 +1,16 @@
+
+# Arithmetic Operators
+
+print(5 + 3) # Addition
+
+print(5 - 3) # Subtraction
+
+print(5 * 3) # Multiplication
+
+print(5 / 2) # Division
+
+print(5 // 2) # Floor Division
+
+print(5 % 2) # Modulus(Remainder)
+
+print(5 ** 2) # Exponentiation
