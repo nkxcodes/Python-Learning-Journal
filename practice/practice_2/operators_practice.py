aaf7ad1b98e3cf288be567adfdd340ce1c73fb3f
Nitesh <niteshkjaiswal108@gmail.com>
2026-06-27 12:11:53 +0530

    Add additional examples of division and modulus operations to operators_practice.py, including predictions of outputs for various arithmetic expressions to enhance understanding of mathematical concepts in Python.

--- practice/practice_2/operators_practice.py
+++ practice/practice_2/operators_practice.py
@@ -19,3 +19,12 @@ print(25 // 4) # Output: 6
 print(25 % 4) # Output: 1
 
 print(2 ** 6)  # Output 64
+
+# Predict every output
+    # Without running
+
+print(20 // 3) # Output: 6
+
+print(20 / 3) # Output: 6.666
+
+print(20 % 3) # Output: 2
