6d88e8d0206a1f9eceb02db89288cebb4533e7ee
Nitesh <niteshkjaiswal108@gmail.com>
2026-06-27 12:08:37 +0530

    Add additional arithmetic operation examples to operators_practice.py, including subtraction, multiplication, division, floor division, modulus, and exponentiation, to further enhance the understanding of basic mathematical concepts in Python.

--- practice/practice_2/operators_practice.py
+++ practice/practice_2/operators_practice.py
@@ -5,3 +5,17 @@ print(10 + 5 * 2) # Output: 20
 print(20 // 3) # Output: 6
 
 print(20 % 3) # Output: 6.6 # Not Clear
+
+print(15 + 7) # Output: 22
+
+print(20 - 9) # Output: 11
+
+print(12 * 8) # Output: 96
+
+print(25 / 4) # Output: 6.25
+
+print(25 // 4) # Output: 6
+
+print(25 % 4) # Output: 1
+
+print(2 ** 6)  # Output 64
