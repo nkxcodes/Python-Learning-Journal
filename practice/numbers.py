69d9ef94170e853931938c37b6b457c25411836a
Nitesh <niteshkjaiswal108@gmail.com>
2026-06-27 17:44:04 +0530

    Enhance numbers.py with output comments for arithmetic operations, improving clarity and understanding of results in Python.

--- practice/numbers.py
+++ practice/numbers.py
@@ -1,26 +1,26 @@
 
-print(2 + 2)
+print(2 + 2) # Output: 4
 
-print(50 - 5 * 6)
+print(50 - 5 * 6) # Output: 20
 
-print((50 - 5 * 6) / 4)
+print((50 - 5 * 6) / 4) # Output: 5.0
 
-print(8 / 5)
+print(8 / 5) # Output: 1.6
 
-print(17 / 3)
+print(17 / 3) # Output: 5.666666666666667
 
-print(17 // 3)
+print(17 // 3) # Output: 5
 
-print(17 % 3)
+print(17 % 3) # Output: 2
 
-print(5 * 3 + 1)
+print(5 * 3 + 1) # Output: 16
 
-print(5 ** 2)
+print(5 ** 2) # Output: 25
 
-print(2 ** 7)
+print(2 ** 7) # Output: 128
 
 width = 20
 height = 5 * 9
-print(width * height)
+print(width * height) # Output: 900
 
-print(4 * 3.75 -1)
+print(4 * 3.75 -1) # Output: 14.0
