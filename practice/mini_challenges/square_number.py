69cfc13e2863db50b5fe2c86991cdc66a048b32d
Nitesh <niteshkjaiswal108@gmail.com>
2026-06-28 19:28:34 +0530

    Add square_number.py to calculate the square of a user-provided number, enhancing understanding of arithmetic operations in Python.

---
+++ practice/mini_challenges/square_number.py
@@ -0,0 +1,16 @@
+# Square Calculator
+#
+# Description:
+# Calculate the square of a number using an operator
+#
+# Example:
+# Enter a number: 7
+#
+# Output: 
+# Square: 49
+
+number = int(input('Enter a Number: '))
+
+square_num = 7 ** 2
+
+print(f'Square: {square_num}')
