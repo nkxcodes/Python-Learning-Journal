e4be432cee3a4493f55e3a837a3e1585b90edc29
Nitesh <niteshkjaiswal108@gmail.com>
2026-06-28 19:33:23 +0530

    Add cube_number.py to calculate the cube of a user-provided number, enhancing understanding of arithmetic operations in Python.

---
+++ practice/mini_challenges/cube_number.py
@@ -0,0 +1,16 @@
+# Cube Calculator
+#
+# Description:
+# Calculate the cube of a number using an operator.
+#
+# Example:
+# Enter a number: 4
+#
+# Output:
+# Cube: 64
+
+number = int(input('Enter a Number: '))
+
+cube_num = number ** 3
+
+print(f'Cube: {cube_num}')
