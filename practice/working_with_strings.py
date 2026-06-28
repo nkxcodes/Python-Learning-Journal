2bdabc6939956f894b2c0fe64c32d9afbc1b8db9
Nitesh <niteshkjaiswal108@gmail.com>
2026-06-28 19:54:49 +0530

    Add working_with_strings.py to demonstrate string manipulation techniques, including length calculation and case conversion, enhancing understanding of string operations in Python.

---
+++ practice/working_with_strings.py
@@ -0,0 +1,14 @@
+
+name = input('Enter your Name: ')
+
+
+result_1 = len(name) # Used to find numbers of characters there in a string
+result_2 = name.capitalize() # Used to capitalize the first letter of the string
+result_3 = name.upper() # Used to capitalize all characters there in a string
+result_4 = name.lower() # Used to decapitalize all characters there in a string
+
+
+print(result_1)
+print(result_2)
+print(result_3)
+print(result_4)
