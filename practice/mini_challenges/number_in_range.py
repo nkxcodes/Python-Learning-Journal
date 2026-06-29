0caa53f9d103d6b20a5818329b946cefda6fad3c
Nitesh <niteshkjaiswal108@gmail.com>
2026-06-29 18:31:54 +0530

    Add number_in_range.py to determine if a user-provided number lies within a specified range, enhancing understanding of conditional statements in Python.

---
+++ practice/mini_challenges/number_in_range.py
@@ -0,0 +1,19 @@
+# Number Range Checker
+#
+# Description:
+# Determine whether a number lies within a specified range.
+#
+# Example:
+# Enter a number: 18
+#
+# Output:
+# The number is within the range.
+
+number = int(input('Enter a number: '))
+
+range = 20
+
+if number <= 20:
+    print('The number is within range')
+else: 
+    print('The number is not within range')
