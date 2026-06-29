5f17057a17a262df80f445409bca5846b468a70e
Nitesh <niteshkjaiswal108@gmail.com>
2026-06-29 20:42:38 +0530

    Add string_case_checker.py to evaluate if a string is entirely uppercase or lowercase, enhancing understanding of string methods in Python.

--- practice/working_with_strings.py
+++ practice/working_with_strings.py
@@ -6,7 +6,8 @@ result_1 = len(name) # Used to find numbers of characters there in a string
 result_2 = name.capitalize() # Used to capitalize the first letter of the string
 result_3 = name.upper() # Used to capitalize all characters there in a string
 result_4 = name.lower() # Used to decapitalize all characters there in a string
-
+result_5 = name.isupper() # Used to check is whole string is in upppercase
+result_6 = name.islower() # Used to check is whole string is in lowercase
 
 print(result_1)
 print(result_2)
