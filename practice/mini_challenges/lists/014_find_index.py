490f207ec6ac88faa842a7ab5d7c15ba3c9a55d0
Nitesh <niteshkjaiswal108@gmail.com>
2026-06-30 12:32:45 +0530

    Add 014_find_index.py to find the index of a given value in a list, enhancing understanding of list indexing in Python.

---
+++ practice/mini_challenges/lists/014_find_index.py
@@ -0,0 +1,7 @@
+# Question:
+# Find the index of a given value.
+
+fruits = ['Apple', 'Mango', 'Banana', 'Pineapple', 'Avacado']
+
+
+print(fruits.index('Pineapple')) # .index feature is used to find index of a given value
