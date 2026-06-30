3d4b4070132b7f5ffa64a068648d08e6ac2d66d5
Nitesh <niteshkjaiswal108@gmail.com>
2026-06-30 12:28:12 +0530

    Add 012_check_item_exists.py to check for the existence of a value in a list, enhancing understanding of membership testing in Python.

---
+++ practice/mini_challenges/lists/012_check_item_exists.py
@@ -0,0 +1,13 @@
+# Question:
+# Check whether a given value exists in the list.
+
+fruits = ['Apple', 'Mango', 'Banana', 'Pineapple', 'Avacado']
+
+if 'Pineapple' in fruits:
+    print('Exists')
+else:
+    print('Not Exists!')
+
+print('Mango' in fruits)
+
+print('Guava' in fruits)
