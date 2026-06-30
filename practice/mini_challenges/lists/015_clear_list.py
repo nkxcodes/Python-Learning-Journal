f35528c1638ebefef50a231ebcde8b92c252353f
Nitesh <niteshkjaiswal108@gmail.com>
2026-06-30 12:34:53 +0530

    Add 015_clear_list.py to demonstrate how to remove all elements from a list using the .clear() method, enhancing understanding of list manipulation in Python.

---
+++ practice/mini_challenges/lists/015_clear_list.py
@@ -0,0 +1,8 @@
+# Question:
+# Remove all elements from the list.
+
+fruits = ['Apple', 'Mango', 'Banana', 'Pineapple', 'Avacado']
+
+fruits.clear() # .clear feature is used to clear list's all values without removing the list, it just removes the values inside a given list
+
+print(fruits)
