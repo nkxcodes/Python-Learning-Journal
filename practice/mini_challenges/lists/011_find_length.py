c904fd8add2e650433fe89cfc5587926f2f9cc91
Nitesh <niteshkjaiswal108@gmail.com>
2026-06-30 12:24:09 +0530

    Add 011_find_length.py to print the total number of elements in a list, enhancing understanding of list length calculation in Python.

---
+++ practice/mini_challenges/lists/011_find_length.py
@@ -0,0 +1,8 @@
+# Question:
+# Print the total number of elements in the list.
+
+fruits = ['Apple', 'Mango', 'Banana', 'Pineapple', 'Avacado']
+
+length_of_fruits = len(fruits)
+
+print(length_of_fruits)
