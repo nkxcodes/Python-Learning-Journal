dee465276ed96bf56ebaead36fb3808ab8e8b2bc
Nitesh <niteshkjaiswal108@gmail.com>
2026-07-01 15:36:28 +0530

    Add 036_copy_list.py to create a copy of a list using the copy() method, enhancing understanding of list duplication in Python.

---
+++ practice/mini_challenges/lists/modification/036_copy_list.py
@@ -0,0 +1,11 @@
+# Create a copy of the list
+
+fruits = [
+    "apple", "banana", "mango", "orange", "grapes",
+    "kiwi", "pear", "watermelon", "papaya", "guava"
+]
+
+new_fruits = fruits.copy()
+
+print(f'Fruits: {fruits}')
+print(f'New Fruits: {new_fruits}')
