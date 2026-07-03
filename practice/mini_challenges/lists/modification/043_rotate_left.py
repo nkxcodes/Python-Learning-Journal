fd18f090baf012d57b52db6c60d6736d612f9d6f
Nitesh <niteshkjaiswal108@gmail.com>
2026-07-03 17:19:04 +0530

    Implement left rotation of a list in 043_rotate_left.py, enhancing understanding of list manipulation techniques in Python.

--- practice/mini_challenges/lists/modification/043_rotate_left.py
+++ practice/mini_challenges/lists/modification/043_rotate_left.py
@@ -1,3 +1,13 @@
 # Rotate the list left by one position
 
 numbers = [14, 27, 35, 8, 19, 42, 6, 51]
+
+saved_num = numbers[0]
+
+for index in range(0, len(numbers) - 1):
+    numbers[index] = numbers[index + 1]
+
+last_position = len(numbers) - 1
+numbers[last_position] = saved_num
+
+print(numbers)
