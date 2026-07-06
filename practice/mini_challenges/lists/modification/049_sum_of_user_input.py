7d063539664136ee08298e46e6d8cff823b730b5
Nitesh <niteshkjaiswal108@gmail.com>
2026-07-06 19:38:44 +0530

    Implement user input handling in 049_sum_of_user_input.py to allow users to enter numbers, store them in a list, and calculate the total sum, enhancing understanding of loops and list operations in Python.

--- practice/mini_challenges/lists/modification/049_sum_of_user_input.py
+++ practice/mini_challenges/lists/modification/049_sum_of_user_input.py
@@ -1,4 +1,21 @@
 # Question:
 # Ask the user to enter numbers, store them in a list, and calculate the sum of all numbers.
 
-print()
+print('Enter 0 to End the list: ')
+
+numbers = []
+
+is_running = True 
+
+sum = 0
+
+while is_running:
+    num = int(input('Enter Number: '))
+    if num == 0:
+        is_running = False
+    numbers.append(num)
+
+for num in numbers:
+    sum += num
+
+print(f'Sum: {sum}')
