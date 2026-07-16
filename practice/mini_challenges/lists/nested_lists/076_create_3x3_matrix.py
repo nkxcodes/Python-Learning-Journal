08a087f1ab11cbac616a0ab05e0c425ca22909f8
Nitesh <niteshkjaiswal108@gmail.com>
2026-07-16 21:07:53 +0530

    Add 076_create_3x3_matrix.py to create and print a 3×3 matrix using nested lists, enhancing understanding of list structures in Python.

---
+++ practice/mini_challenges/lists/nested_lists/076_create_3x3_matrix.py
@@ -0,0 +1,7 @@
+# Question:
+# Create a 3×3 matrix (nested list) and print it.
+
+matrix_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] # Creates matrix list
+
+for row in matrix_list: # Loops through the matrix list array and prints the rows
+    print(row)
