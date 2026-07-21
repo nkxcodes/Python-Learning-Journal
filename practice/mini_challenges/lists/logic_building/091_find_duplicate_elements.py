ec548b3b382bef9907828ef6d48592167e0553b2
Nitesh <niteshkjaiswal108@gmail.com>
2026-07-21 22:21:39 +0530

    Add 091_find_duplicate_elements.py to find and print all duplicate elements in a list, enhancing understanding of list operations in Python.

---
+++ practice/mini_challenges/lists/logic_building/091_find_duplicate_elements.py
@@ -0,0 +1,17 @@
+# Question:
+# Find and print all duplicate elements in a list.
+
+numbers = [10, 20, 30, 40, 50, 60, 70, 10, 30, 40, 50, 40, 99, 980]
+
+original_numbers = []
+
+dup_numbers = []
+
+for number in numbers:
+    if number in original_numbers:
+        dup_numbers.append(number)
+    else:
+        original_numbers.append(number)
+
+print(dup_numbers)
+print(original_numbers)
