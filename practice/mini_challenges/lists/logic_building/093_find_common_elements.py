ec1725a722c7c229550591cd0d63dea36236a7cf
Nitesh <niteshkjaiswal108@gmail.com>
2026-07-22 22:22:48 +0530

    Add logic in 093_find_common_elements.py to handle duplicates when finding common elements between two lists, improving the accuracy of the comparison and enhancing the overall functionality.

--- practice/mini_challenges/lists/logic_building/093_find_common_elements.py
+++ practice/mini_challenges/lists/logic_building/093_find_common_elements.py
@@ -1,6 +1,9 @@
 # Question:
 # Find and print the common elements between two lists.
 
+from multiprocessing import process
+
+
 list1 = [1, 2, 3, 4, 5]
 list2 = [3, 5, 7, 9]
 
@@ -34,3 +37,16 @@ for number in list1:
 
 if not found:
     print('No common elements found.')
+
+list1 = [1, 2, 2, 3, 4, 5]
+list2 = [2, 2, 4, 6]
+
+processed = []
+
+for number in list1:
+    if number in list2:
+        if number in processed:
+            continue
+        else:
+            print(number)
+            processed.append(number)
