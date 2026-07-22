c9d5d78d4fb73900490e904241f1aaf5db2f2672
Nitesh <niteshkjaiswal108@gmail.com>
2026-07-22 22:09:15 +0530

    Refactor 093_find_common_elements.py to improve functionality by using list membership for finding common elements between two lists, enhancing clarity and efficiency in list comparison.

--- practice/mini_challenges/lists/logic_building/093_find_common_elements.py
+++ practice/mini_challenges/lists/logic_building/093_find_common_elements.py
@@ -1,9 +1,9 @@
 # Question:
 # Find and print the common elements between two lists.
 
-num_1 = [10, 20, 30, 40, 50, 5, 6]
-num_2  = [1, 2, 3, 4, 5, 5, 6]
+list1 = [1, 2, 3, 4, 5]
+list2 = [3, 5, 7, 9]
 
-for index in range(0, len(num_1)):
-    if num_1[index] == num_2[index]:
-        print(num_1)
+for number in list1:
+    if number in list2:
+        print(number)
