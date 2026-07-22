345edaaaa20532f7c4e39f5bd93f393eefa8bb02
Nitesh <niteshkjaiswal108@gmail.com>
2026-07-22 22:10:36 +0530

    Add additional test cases in 093_find_common_elements.py to demonstrate finding common elements between multiple lists, improving the robustness of the functionality.

--- practice/mini_challenges/lists/logic_building/093_find_common_elements.py
+++ practice/mini_challenges/lists/logic_building/093_find_common_elements.py
@@ -7,3 +7,10 @@ list2 = [3, 5, 7, 9]
 for number in list1:
     if number in list2:
         print(number)
+
+list1 = [10, 20, 30, 40]
+list2 = [20, 40, 60, 80]
+
+for number in list1:
+    if number in list2:
+        print(number)
