6ce660837c8066855fb7824a809cc5d956f41c39
Nitesh <niteshkjaiswal108@gmail.com>
2026-07-23 23:11:25 +0530

    Add new script 099_shuffle_list.py to randomly shuffle the elements of a list and display the result, enhancing the demonstration of list manipulation in Python.

---
+++ practice/mini_challenges/lists/logic_building/099_shuffle_list.py
@@ -0,0 +1,10 @@
+# Question:
+# Randomly shuffle the elements of a list and display the result.
+
+import random
+
+list1 = ["apple", "banana", "mango", "orange", "grapes"]
+
+random.shuffle(list1)
+
+print(list1)
