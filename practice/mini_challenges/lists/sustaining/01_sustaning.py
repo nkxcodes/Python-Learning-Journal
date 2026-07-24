150f3c9286166a14272cf5c5a6d8c2af663f0107
Nitesh <niteshkjaiswal108@gmail.com>
2026-07-24 23:38:45 +0530

    Add functionality to 01_sustaning.py to find the smallest element in a new list, enhancing the demonstration of list manipulation in Python.

--- practice/mini_challenges/lists/sustaining/01_sustaning.py
+++ practice/mini_challenges/lists/sustaining/01_sustaning.py
@@ -9,3 +9,14 @@ for element in list1:
         largest_element = element
 
 print(f'Largest Element: {largest_element}')
+
+
+list2 = [10, 20, 30, 40, 50, 60]
+
+smallest_element =  list2[0]
+
+for element in list2:
+    if element < smallest_element:
+        smallest_element = element
+
+print(f'Smallest Element: {smallest_element}')
