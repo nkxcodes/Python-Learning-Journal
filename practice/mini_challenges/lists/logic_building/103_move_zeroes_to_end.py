e7312aabec6b4bb02af0aef939cb1b7dd2f40634
Nitesh <niteshkjaiswal108@gmail.com>
2026-07-28 21:10:54 +0530

    Implement logic to move all zero values to the end of the list in 103_move_zeroes_to_end.py, maintaining the order of non-zero elements, enhancing list manipulation techniques in Python.

--- practice/mini_challenges/lists/logic_building/103_move_zeroes_to_end.py
+++ practice/mini_challenges/lists/logic_building/103_move_zeroes_to_end.py
@@ -1,3 +1,18 @@
 # Question:
 # Move all zero values to the end of the list while keeping the order of the other elements unchanged.
 
+list1 = [2, 0, 3, 0, 4, 0, 5, 0, 0, 0, 6, 7, 8]
+
+spare_list = []
+
+for index in range(len(list1) - 1, -1, -1):
+    if list1[index] == 0:
+        spare_list.append(list1[index])
+        list1.remove(list1[index])
+
+for index in range(len(spare_list) - 1, -1, -1):
+    list1.append(spare_list[index])
+    spare_list.pop(spare_list[index])
+
+print(list1)
+print(spare_list)
