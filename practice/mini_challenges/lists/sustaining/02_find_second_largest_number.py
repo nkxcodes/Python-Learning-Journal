28d49ba7de45ab61013e0e66644369c4499c1f9a
Nitesh <niteshkjaiswal108@gmail.com>
2026-07-29 22:23:38 +0530

    Add functionality to print student details in 02_find_second_largest_number.py, enhancing the script's output and demonstrating data handling in Python.

--- practice/mini_challenges/lists/sustaining/02_find_second_largest_number.py
+++ practice/mini_challenges/lists/sustaining/02_find_second_largest_number.py
@@ -10,3 +10,22 @@ for element in numbers:
         second_largest = element
 
 print(f'Second Largest: {second_largest}')
+
+students = [
+    {
+        "name": "Nitesh",
+        "age": "17"
+    },
+    {
+        "name": "Mridul",
+        "age": "17"
+    }
+]
+
+print()
+print(f"Name: {students[0]["name"]}")
+print(f"Age: {students[0]["age"]}")
+
+print()
+print(f"Name: {students[1]["name"]}")
+print(f"Age: {students[1]["age"]}")
