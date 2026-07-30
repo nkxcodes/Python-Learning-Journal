33526b7e679a42c6bc47d37a085e9e2ea5386444
Nitesh <niteshkjaiswal108@gmail.com>
2026-07-30 18:59:57 +0530

    Refactor prime number extraction logic in 108_extract_prime_numbers.py to use index-based iteration, improving clarity and efficiency of the algorithm.

--- practice/mini_challenges/lists/logic_building/108_extract_prime_numbers.py
+++ practice/mini_challenges/lists/logic_building/108_extract_prime_numbers.py
@@ -6,13 +6,14 @@ list1 = [2, 4, 5, 6, 7, 9, 11, 12, 13, 15]
 
 prime_numbers = []
 
-for element in list1:
+
+for index in range(0, len(list1)):
     prime = True
-    for number in range(2, 6):
-        if element % number == 0:
+    for number in range(2, list1[index] - 1):
+        if list1[index] % number == 0:
             prime = False
-            
-    if prime == True:
-        prime_numbers.append(element)
+            break
+    if prime:
+        prime_numbers.append(list1[index])
 
 print(prime_numbers)
