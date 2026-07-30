e9bf7541a179b7371d10d230a31146a08576f47d
Nitesh <niteshkjaiswal108@gmail.com>
2026-07-30 19:20:05 +0530

    Add new script 109_extract_palindrome_numbers.py to extract palindrome numbers from a list, enhancing list manipulation techniques in Python.

---
+++ practice/mini_challenges/lists/logic_building/109_extract_palindrome_numbers.py
@@ -0,0 +1,24 @@
+# Question:
+# Create a new list containing only the palindrome numbers from the original list
+
+list1 = [121, 45, 33, 123, 454, 67, 99, 101, 234, 1221]
+
+palindrome_numbers = []
+
+
+for index in range(0, len(list1)):
+    is_palindrome = False
+    original_number = list1[index]
+    reversed_number = 0
+    while original_number > 0:
+        reversed_number = reversed_number * 10
+        reversed_number += original_number % 10
+        original_number = original_number // 10
+
+    if list1[index] == reversed_number:
+        is_palindrome = True
+
+    if is_palindrome:
+        palindrome_numbers.append(list1[index])
+
+print(palindrome_numbers)
