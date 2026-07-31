3a9dcae650d94c6682e2730c0271d617e9b9b26f
Nitesh <niteshkjaiswal108@gmail.com>
2026-07-31 15:46:17 +0530

    Add functionality to the custom append() method in 111_implement_append.py, allowing for element addition and enhancing list manipulation techniques in Python.

--- practice/mini_challenges/lists/challenge_problems/111_implement_append.py
+++ practice/mini_challenges/lists/challenge_problems/111_implement_append.py
@@ -1,3 +1,13 @@
 # Question:
 # Implement your own version of the append() method without using python's built-in append()
 
+list1 = [10, 20, 30]
+
+new_element = 40
+
+spare_list = []
+
+def append(element):
+    print(element)
+
+append('Hello')
