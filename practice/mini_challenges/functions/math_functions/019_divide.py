d9ffbae5d4f26d1a8871c3d89fce59da33ec6972
Nitesh <niteshkjaiswal108@gmail.com>
2026-08-01 21:27:22 +0530

    Solve (divide.py)  Question

---
+++ practice/mini_challenges/functions/math_functions/019_divide.py
@@ -0,0 +1,14 @@
+# Return division
+
+def divide(num1,  num2):
+    return num1 / num2
+
+def floorDivision(num1, num2):
+    return num1 // num2
+
+division = divide(10, 2)
+
+floor_division = floorDivision(10, 2)
+
+print(division)
+print(floor_division)
