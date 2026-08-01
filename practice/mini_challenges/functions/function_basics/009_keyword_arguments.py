7375ea666935431c5319014612f201fdd6c09a06
Nitesh <niteshkjaiswal108@gmail.com>
2026-08-01 12:32:31 +0530

    Add functionality to 009_keyword_arguments.py by introducing the intro function, demonstrating the use of default parameters and various ways to call functions with keyword arguments.

--- practice/mini_challenges/functions/function_basics/009_keyword_arguments.py
+++ practice/mini_challenges/functions/function_basics/009_keyword_arguments.py
@@ -13,3 +13,13 @@ greet('Bob', country='Canada') # Output: Hello Bob from Canada
 
 greet('Bob', country='Canada') # This is valid
 # greet(name='Bob', 'India') # This gives an error becomes positional arguments cannot come after keyword arguments
+
+def intro(name='Guest', age=18, city='Delhi'):
+    print(name, age, city)
+
+intro() # Output: Guest 18 Delhi
+intro('Rahul') # Output: Rahul 18 Delhi
+intro(age=20) # Output: Guest 20 Delhi
+intro('Rahul', 21) # Output: Rahul 21 Delhi
+intro(city='Mumbai', name='Aman') # Output: Aman 18 Mumbai
+intro('Aman', city='Mumbai') # Output: Aman 18 Mumbai
