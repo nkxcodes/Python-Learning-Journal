f2cad4c4ec4c2f6387039b573b68a8cf6e875f41
Nitesh <niteshkjaiswal108@gmail.com>
2026-08-02 07:59:01 +0530

    Added comment for factorial.py

---
+++ practice/mini_challenges/functions/math_functions/020_power.py
@@ -0,0 +1,8 @@
+# Return a number raised to a power
+
+def power_of(number, raise_to_the_power):
+    return number ** raise_to_the_power
+
+power_of_2_to_3 = power_of(2, 3)
+
+print(power_of_2_to_3)
--- practice/mini_challenges/functions/math_functions/021_factorial.py
+++ practice/mini_challenges/functions/math_functions/021_factorial.py
@@ -1,8 +1,10 @@
+# Return factorial using a function
+
 def factorial_of(number):
     factorial = 0
     for num in range(1, number):
         factorial += num
-
+        
     return factorial
 
 factorial_of_10 = factorial_of(10)
