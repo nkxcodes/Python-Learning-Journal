6ee98997c41db185b38a9f9a7fc4d7dd5a6cc5f9
Nitesh <niteshkjaiswal108@gmail.com>
2026-08-04 17:33:54 +0530

    Change toUpperCase to upper to solve the question

--- practice/mini_challenges/functions/string_functions/035_uppercase.py
+++ practice/mini_challenges/functions/string_functions/035_uppercase.py
@@ -1,6 +1,8 @@
 # Return uppercase version
 
 def convert_to_uppercase(u_string):
-    return u_string.toUpperCase()
+    return u_string.upper()
 
 conversion = convert_to_uppercase('programming')
+
+print(conversion)
