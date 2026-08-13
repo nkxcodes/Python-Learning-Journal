6bc9ad99d89029d970309283df31408ca4b36122
Nitesh <niteshkjaiswal108@gmail.com>
2026-08-13 15:39:45 +0530

    Solve 055_merge_list.py question

---
+++ practice/mini_challenges/functions/list_functions/055_merge_lists.py
@@ -0,0 +1,9 @@
+# Return merged list.
+
+def merge_list(u_list_1, u_list_2):
+    u_list = u_list_1 + u_list_2
+    return u_list
+
+result = merge_list([1, 2, 3, 4, 5], [5, 4, 3, 2, 1])
+
+print(result)
