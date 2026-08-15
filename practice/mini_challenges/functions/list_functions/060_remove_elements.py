536ba8830d301f18411c2e432506c8a643bc6ac6
Nitesh <niteshkjaiswal108@gmail.com>
2026-08-15 16:23:25 +0530

    Add remove_element function to filter out all occurrences of a specified element from a list

---
+++ practice/mini_challenges/functions/list_functions/060_remove_elements.py
@@ -0,0 +1,14 @@
+# Return alloccurences of an element.
+
+def remove_element(u_list, r_element):
+    new_list = []
+    for element in u_list:
+        if element == r_element:
+            continue
+        else:
+            new_list.append(element)
+    return new_list
+
+result = remove_element([10, 20, 30, 20, 40, 20, 50], 20)
+
+print(result)
