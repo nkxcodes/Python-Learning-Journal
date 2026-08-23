a6c9dc2aab0edf99fac3d991dfa642330b8f0e0c
Nitesh <niteshkjaiswal108@gmail.com>
2026-08-23 21:46:13 +0530

    Add multiplication_of_5 function to print a multiplication table of 5

---
+++ practice/mini_challenges/functions_basics_2/08_multiplication_table_of_5.py
@@ -0,0 +1,7 @@
+# Write a function that prints a multiplication table of 5.
+
+def multiplication_of_5():
+    for num in range(1, 11):
+        print(f'5 x {num} = {5 * num}')
+
+multiplication_of_5()
