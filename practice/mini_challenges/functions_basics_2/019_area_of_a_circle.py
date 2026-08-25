6063d9f9797f41327b62f0030faf1a45fa5a3cd4
Nitesh <niteshkjaiswal108@gmail.com>
2026-08-25 22:17:12 +0530

    Add area_of_circle function to calculate and print the area of a circle

---
+++ practice/mini_challenges/functions_basics_2/019_area_of_a_circle.py
@@ -0,0 +1,8 @@
+# Write a function that takes radius and prints the area of a circle.
+
+def area_of_circle(radius):
+    return (22 / 7) * (radius ** 2)
+
+result = area_of_circle(7)
+
+print(result)
