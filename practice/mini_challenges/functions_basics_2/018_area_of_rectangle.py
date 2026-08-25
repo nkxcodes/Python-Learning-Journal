5eadb729769ffec33314207e1a8751d79fcba7d5
Nitesh <niteshkjaiswal108@gmail.com>
2026-08-25 22:14:04 +0530

    Add area_of_rectangle function to calculate and print the area of a rectangle

---
+++ practice/mini_challenges/functions_basics_2/018_area_of_rectangle.py
@@ -0,0 +1,8 @@
+# Write a function that takes length and width and prints the area of the rectangle.
+
+def area_of_rectangle(length, width):
+    return length * width
+
+result = area_of_rectangle(8, 5)
+
+print(result)
