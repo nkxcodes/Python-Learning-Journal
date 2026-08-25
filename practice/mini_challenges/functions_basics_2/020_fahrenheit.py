08ce659096ecb015d9b03752b73071a707d89d30
Nitesh <niteshkjaiswal108@gmail.com>
2026-08-25 22:20:29 +0530

    Add fahrenheit function to convert Celsius to Fahrenheit and print the result

---
+++ practice/mini_challenges/functions_basics_2/020_fahrenheit.py
@@ -0,0 +1,8 @@
+# Write a function that takes a temperature in celsius and prints fahrenheit.
+
+def fahrenheit(c_temperature):
+    return (c_temperature * 9 / 5) + 32
+
+result = fahrenheit(25)
+
+print(result)
