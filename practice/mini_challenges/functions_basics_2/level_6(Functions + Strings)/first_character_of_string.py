09b0014fe097d58ac2cd546352be38f66e640b81
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-11 16:06:46 +0530

    Add first_character function to return the first character of a string

---
+++ practice/mini_challenges/functions_basics_2/level_6(Functions + Strings)/first_character_of_string.py
@@ -0,0 +1,8 @@
+# Write a function that returns the first character of a string.
+
+def first_character(u_string):
+    return u_string[0]
+
+result = first_character("Programming")
+
+print(result)
