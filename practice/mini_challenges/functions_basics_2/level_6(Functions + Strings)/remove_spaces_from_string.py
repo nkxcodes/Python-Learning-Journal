8a84c66e6f496268bdd3ff207352344d8ed5f759
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-13 20:49:51 +0530

    Add remove_spaces function to eliminate spaces from a string

---
+++ practice/mini_challenges/functions_basics_2/level_6(Functions + Strings)/remove_spaces_from_string.py
@@ -0,0 +1,8 @@
+# Write a function that removes spaces from string.
+
+def remove_spaces(u_string):
+    return u_string.replace(' ', '')
+
+result = remove_spaces('I love python programming')
+
+print(result)
