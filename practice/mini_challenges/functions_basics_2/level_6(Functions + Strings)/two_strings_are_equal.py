f52d0805b8984674dbd1a4e387822201f1caa971
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-13 21:44:49 +0530

    Add is_equal function to check if two strings are equal

---
+++ practice/mini_challenges/functions_basics_2/level_6(Functions + Strings)/two_strings_are_equal.py
@@ -0,0 +1,8 @@
+# Write a function that checks whether two strings are equal.
+
+def is_equal(u_string_1, u_string_2):
+    return u_string_1 == u_string_2
+
+result = is_equal('Python', 'Python')
+
+print(result)
